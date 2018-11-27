c99b6647509cfe1f6563350a42661ffb80a810aa
Kakeru Ishii <i.kakeru@grimoire.gl>
2018-11-27 13:14:46 +0900

    fix cover all script

--- packages/api-schema/__tests__/api-schema.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-describe('@firebase-monorepo/api-schema', () => {
-    it('needs tests',()=>{
-        console.log("THIS IS TEST")
-    });
-});
---
+++ packages/api-schema/src/__tests__/api-schema.test.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const counter = require('../index')
+describe('@firebase-monorepo/api-schema', () => {
+  it('needs tests', () => {
+    console.log('THIS IS TEST')
+  })
+})
